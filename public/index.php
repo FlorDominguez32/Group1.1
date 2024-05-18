fca08459b74dc639d2b50e4f89cbc3334a432625
FlorDominguez32 <146401735+FlorDominguez32@users.noreply.github.com>
2024-05-18 12:23:49 -0600

    log in

--- public/index.php
+++ public/index.php
@@ -1,17 +1,38 @@
-<?php
 
-use Illuminate\Http\Request;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
 
-define('LARAVEL_START', microtime(true));
+    <title>Document</title>
+</head>
+<div class="bg-gray-200 mt-28 text-center">Bienvenido</div>
+<body>
+    <div class=" h-20 w-3/4 p-4 p-60 mx-80 my-44 mt-44
+    md:w-2/4 rounded-lg bg-gray-200  pt-44 pb-36 justify-center flex flex-col ">
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+    <h1 class="p-4 ">Log in</h1>
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+        <div>
+            <label>Email:</label>
+            <input id="emailI" class="rounded-full border border-black">
+        </div>
+
+        <div>
+            <label>Contraseña:</label>
+            <input id="passwordI" class="rounded-full border border-black">
+        </div>
+
+        <div class="p-3 ">
+            <button type="submit" onclick="guardarEnStorage()" class="rounded-full relative w-44 h-5 bg-white-600 ">Enviar</button>
+        </div>
+    </div>
+
+</body>
+
+</html>
+
+
